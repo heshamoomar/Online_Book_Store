84906ceab1fef7acb58b4da1048250887986e78a
Hesham Omar <hesham.omar1122@gmail.com>
2023-04-04 22:40:42 +0200

    my commit

---
+++ Online_Book_Store/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Online_Book_Store.Startup))]
+namespace Online_Book_Store
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
